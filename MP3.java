12aab89092e6fac95b550b5ff3b33a35310eafbc
lm2590025 <lm2590025@cityu.ac.cy>
2026-05-09 23:09:05 +0600

    Add MP3.java

---
+++ MP3.java
@@ -0,0 +1,46 @@
+/**
+ * MP3.java
+ * Subclass of Gadget representing an MP3 player.
+ * Student: Md Mirazul Islam Rahat
+ * Student ID: 221LM2590025
+ */
+
+public class MP3 extends Gadget {
+    private int availableMemory;
+
+    public MP3(String model, double price, int weight, String size, int availableMemory) {
+        super(model, price, weight, size);
+        this.availableMemory = availableMemory;
+    }
+
+    public int getAvailableMemory() {
+        return availableMemory;
+    }
+
+    public void downloadMusic(int memoryUsed) {
+        if (availableMemory >= memoryUsed) {
+            availableMemory = availableMemory - memoryUsed;
+            System.out.println(memoryUsed + " MB music downloaded successfully.");
+            System.out.println("Remaining memory: " + availableMemory + " MB");
+        } else {
+            System.out.println("Not enough memory available to download music.");
+        }
+    }
+
+    public void deleteMusic(int memoryFreed) {
+        availableMemory = availableMemory + memoryFreed;
+        System.out.println(memoryFreed + " MB music deleted successfully.");
+        System.out.println("Available memory: " + availableMemory + " MB");
+    }
+
+    public void display() {
+        super.display();
+        System.out.println("Available Memory: " + availableMemory + " MB");
+    }
+
+    public String toString() {
+        return "MP3 Player\n" +
+               super.toString() +
+               "\nAvailable Memory: " + availableMemory + " MB";
+    }
+}
