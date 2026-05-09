3af955e90d3a7d1ef8b6b1e5f634214d8f452841
lm2590025 <lm2590025@cityu.ac.cy>
2026-05-09 23:08:10 +0600

    Add Mobile.java

---
+++ Mobile.java
@@ -0,0 +1,48 @@
+/**
+ * Mobile.java
+ * Subclass of Gadget representing a mobile phone.
+ * Student: Md Mirazul Islam Rahat
+ * Student ID: 221LM2590025
+ */
+
+public class Mobile extends Gadget {
+    private int callingCredit;
+
+    public Mobile(String model, double price, int weight, String size, int callingCredit) {
+        super(model, price, weight, size);
+        this.callingCredit = callingCredit;
+    }
+
+    public int getCallingCredit() {
+        return callingCredit;
+    }
+
+    public void addCredit(int amount) {
+        if (amount > 0) {
+            callingCredit = callingCredit + amount;
+            System.out.println(amount + " minutes of credit added.");
+        } else {
+            System.out.println("Please enter a positive amount of credit.");
+        }
+    }
+
+    public void makeCall(String phoneNumber, int duration) {
+        if (callingCredit >= duration) {
+            System.out.println("Calling " + phoneNumber + " for " + duration + " minutes.");
+            callingCredit = callingCredit - duration;
+        } else {
+            System.out.println("Insufficient calling credit to make the call.");
+        }
+    }
+
+    public void display() {
+        super.display();
+        System.out.println("Calling Credit: " + callingCredit + " minutes");
+    }
+
+    public String toString() {
+        return "Mobile Phone\n" +
+               super.toString() +
+               "\nCalling Credit: " + callingCredit + " minutes";
+    }
+}
